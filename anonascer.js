69cf6be5b226000e06bd5301730d9f08aad6ce81
Leal <90978901+Lealw@users.noreply.github.com>
2021-10-27 23:00:07 -0300

    Add files via upload

---
+++ anonascer.js
@@ -0,0 +1,9 @@
+
+const nomea = prompt('Qual o seu primeiro nome?');
+const nomeb = prompt('Qual o seu segundo nome?');
+const nasc = prompt('Em qual ano você nasceu?');
+
+const ano = 2021;
+const yano = ano - nasc;
+
+alert(`Olá ${nomea} ${nomeb}, você fez ou vai fazer ${yano} anos esse ano.`)
